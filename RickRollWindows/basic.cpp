48792e1fdb5c55774b3b6ad456d2e81cd5f2a8e3
mrnascara <mrnascara@gmail.com>
2026-02-28 01:36:14 -0800

    Add files via upload
    Windows is here!

---
+++ RickRollWindows/basic.cpp
@@ -0,0 +1,9 @@
+//template
+
+#include <stdio.h>
+
+int main() {
+    printf("never gonna give you up,never gonna let ya down,say goodbye!/n");
+    printf("ARE YOU SERIOUS,STUPID RICKROLL/n");
+    getchar();
+}
